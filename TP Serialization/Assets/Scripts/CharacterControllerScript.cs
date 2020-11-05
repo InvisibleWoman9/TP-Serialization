790e1f911e8d7290b5580e85315ed8127dfeb5b8
InvisibleWoman9 <fanny.beziat34@gmail.com>
2020-11-05 15:06:35 +0100

    Add save3.file

--- TP Serialization/Assets/Scripts/CharacterControllerScript.cs
+++ TP Serialization/Assets/Scripts/CharacterControllerScript.cs
@@ -7,13 +7,21 @@ public class CharacterControllerScript : MonoBehaviour
     public CharacterController controller;
     Transform cubetouche;
     public Collider zonededetection;
+    public Transform camera;
 
     public float speed = 12f;
     public float gravity = -9.81f;
 
     Vector3 velocity;
+    float xRotation = 0f;
+    public float mouseSensitivity = 100f;
 
-
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    
     void Update()
     {
         float x = Input.GetAxis("Horizontal");
@@ -34,19 +42,37 @@ public class CharacterControllerScript : MonoBehaviour
 
         //controller.Move(move * speed * Time.deltaTime);
         
-        //Si le joueur appuie sur la touche espace, le cube detecter par son box collider sera soulevé au même niveau que la zonededetection 
-        if (Input.GetButtonDown("Jump") &&  cubetouche != null)
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        camera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+        
+        //Si le joueur appuie sur le clic de la souris, le cube detecter par son box collider sera soulevé au même niveau que la zonededetection 
+        if (Input.GetMouseButtonDown(0) &&  cubetouche != null)
         {
             cubetouche.GetComponent<Rigidbody>().isKinematic = true;
-            cubetouche.parent = this.transform;
-            cubetouche.position = zonededetection.transform.position; 
+            cubetouche.parent = camera;
+            cubetouche.position = zonededetection.transform.position + zonededetection.transform.forward; 
         }
 
-        //Si le joueur relache la touche espace, le cube dans les "bras" du personnage sera relaché
-        if (Input.GetButtonUp("Jump") &&  cubetouche != null)
+        //Si le joueur relache le clic de la souris, le cube dans les "bras" du personnage sera relaché
+        if (Input.GetMouseButtonUp(0) &&  cubetouche != null)
         {
             cubetouche.parent = null;
             cubetouche.GetComponent<Rigidbody>().isKinematic = false;
+            cubetouche = null;
+        }
+
+        if (Input.GetMouseButton(0) && Input.GetMouseButtonDown(1) && cubetouche != null)
+        {
+            cubetouche.parent = null;
+            cubetouche.GetComponent<Rigidbody>().isKinematic = false;
+            cubetouche.GetComponent<Rigidbody>().AddForce(camera.transform.forward * 10,ForceMode.VelocityChange);
+            cubetouche = null;
         }
 
     }//FIN UPDATE
@@ -71,7 +97,7 @@ public class CharacterControllerScript : MonoBehaviour
     
     void OnTriggerExit(Collider objetrelache)
     {
-        if(objetrelache.transform == cubetouche)
+        if(objetrelache.transform == cubetouche && Input.GetMouseButton(0) == false)
         {
             cubetouche = null;
 
---
+++ TP Serialization/Assets/Scripts/SphereScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereScript : MonoBehaviour
+{
+    Color myColor;
+    public GameObject particles;
+    private List<Collider> objetstouches;
+    
+    void Start()
+    {
+        myColor = GetComponent<MeshRenderer>().material.color;
+        objetstouches = new List<Collider>();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("Player") || col.CompareTag("MOVETHIS"))
+        {
+            col.GetComponent<MeshRenderer>().material.color = myColor;
+            objetstouches.Add(col);
+        }
+    }
+    
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.CompareTag("Player") || col.CompareTag("MOVETHIS"))
+        {
+            objetstouches.Remove(col);
+        }
+    }
+    
+    
+    
+    
+}
